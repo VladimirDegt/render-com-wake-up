a87ad1ee34ac0230eda92c8c642d98d64062e4ef
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-07-08 09:53:12 +0300

    check health

---
+++ src/health/health.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get } from '@nestjs/common';
+import { HealthCheck, HealthCheckService, HttpHealthIndicator } from '@nestjs/terminus';
+import * as process from "node:process";
+
+@Controller('health')
+export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator
+  ) {}
+
+  @Get()
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('check server', process.env.SERVER_URL),
+    ]);
+  }
+}

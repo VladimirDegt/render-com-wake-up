4383f562f8aeade2d512b08306eeb1aa850bc314
Vladimir <107486166+VladimirDegt@users.noreply.github.com>
2024-07-08 10:59:59 +0300

    fix url

--- src/cron.service.ts
+++ src/cron.service.ts
@@ -6,7 +6,7 @@ import * as process from "node:process";
 export class CronService{
 @Cron('*/14 * * * *')
   async handleCron() {
-    const response = await fetch(process.env.SERVER_URL + 'health');
+    const response = await fetch(process.env.SERVER_URL + '/health');
     console.log('виконання розкладу крону');
     console.log(await response.json());
   }
